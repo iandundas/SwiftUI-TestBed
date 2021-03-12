e11d0832526ae6455af7879772e86465c8a1d90a
Ian Dundas <contact@iandundas.com>
2021-03-12 12:26:34 +0100

    Initial Commit

---
+++ TestBed/TestBedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBedApp.swift
+//  TestBed
+//
+//  Created by Ian Dundas on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestBedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
